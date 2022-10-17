db9884be0c33710c5e3bd1eb5609d54c10afdd76
Yanix123 <nanidgor@gmail.com>
2022-10-17 23:58:10 +0300

    fixed js file

--- asd.js
+++ asd.js
@@ -1,27 +1,43 @@
-function validateEmail(email) {
-    var re = /\S+@\S+\.\S+/;
+document.addEventListener("DOMContentLoaded", function (event) {
+  function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
     return re.test(email);
   }
-  
-  window.addEventListener("DOMContentLoaded", function () {
-    const b = this.localStorage.getItem("email");
-  
-    document.getElementById("submit").onclick = function () {
-      console.log('submit1')
-      const a = document.getElementById("email").value;
-      console.log("a", a);
-      console.log('localStorage.getItem("email")', localStorage.getItem("email"));
-      if (!validateEmail(a)) {
-        window.alert(`Email is not valid: ${a}`);
-        return;
+ 
+  document.getElementById("submit").onclick = function (e) {
+    e.preventDefault();
+    const pass = document.getElementById("pass").value;
+    const passconf = document.getElementById("confpass").value;
+    const email = document.getElementById("email").value;
+ 
+    if (email == "") {
+      alert("enter email");
+    } else if (!validateEmail(email)) {
+      alert("email is not valid");
+    } else if (pass == "") {
+      alert("enter password");
+    } else if (passconf == "") {
+      alert("confirm password");
+    } else if (pass != passconf) {
+      alert("Passwords do not match");
+    } else if (validateEmail(email) && pass == passconf) {
+      const allExistingEmailsArray =
+        JSON.parse(localStorage.getItem("emails")) || [];
+      let isSuchEmailAlreadyExists = false;
+      allExistingEmailsArray.forEach((existingEmail) => {
+        if (existingEmail === email) {
+          isSuchEmailAlreadyExists = true;
+        }
+      });
+      if (isSuchEmailAlreadyExists) {
+        return alert("email is already used");
       }
-      if (a === localStorage.getItem("email")) {
-        window.alert("already used");
-      } else {
-        window.alert("you're registred ");
-        
-        localStorage.setItem("email", a);
-      }
-  
-    };
-  });
+      allExistingEmailsArray.push(email);
+      localStorage.setItem("emails", JSON.stringify(allExistingEmailsArray));
+      alert("you are registred ");
+    }
+  };
+});
+    
+
+   
